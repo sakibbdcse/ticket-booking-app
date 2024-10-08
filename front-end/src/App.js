7ed169f80854e1f58d6a5a2cf4ff9d3ddc509e90
sakib <sakibbd.cse@gmail.com>
2024-10-09 00:40:58 +0600

    wellcome to online ticket bokkint system app

---
+++ front-end/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Wellcome to ticket bokking apps vurtualy make you eusy life
+    </div>
+  );
+}
+
+export default App;
